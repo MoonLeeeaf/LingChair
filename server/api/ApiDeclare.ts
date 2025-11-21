578b3507fdf5ccc022adb11d720cec09322c9234
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-11-21 23:44:52 +0800

    更新接口定义

--- server/api/ApiDeclare.ts
+++ server/api/ApiDeclare.ts
@@ -1,40 +1,59 @@
 export type CallMethod = 
+    // 用户验证
     "User.auth" |
     "User.register" |
     "User.login" |
     "User.refreshAccessToken" |
 
+    // 账号
     "User.setAvatar" |
     "User.updateProfile" |
     "User.getMyInfo" |
     "User.resetPassword" |
 
+    // 用户资料
     "User.getInfo" |
 
+    // 收藏对话列表
     "User.getMyContacts" |
     "User.addContacts" |
     "User.removeContacts" |
 
+    // 最近对话列表
     "User.getMyRecentChats" | 
 
+    // 对话信息
     "Chat.getInfo" |
+    "Chat.getAnotherUserIdFromPrivate" |
+    "Chat.getMembers" | 
     
+    // 对话设置
     "Chat.updateSettings" |
     "Chat.setAvatar" |
 
+    // 对话创建
     "Chat.createGroup" |
-
     "Chat.getIdForPrivate" |
-    "Chat.getAnotherUserIdFromPrivate" |
 
+    // 入群请求
     "Chat.processJoinRequest" |
     "Chat.sendJoinRequest" |
     "Chat.getJoinRequests" |
 
+    // 对话消息
     "Chat.sendMessage" |
-    "Chat.getMessageHistory" |
+    "Chat.getMessageHistory"
 
-    "Chat.uploadFile"
+    // (废弃) 文件上传
+    // "Chat.uploadFile"
 
 export type ClientEvent = 
+    // 对话收消息
     "Client.onMessage"
+
+export const CallableMethodBeforeAuth = [
+    "User.auth",
+    "User.register",
+    "User.login",
+    "User.refreshAccessToken",
+]

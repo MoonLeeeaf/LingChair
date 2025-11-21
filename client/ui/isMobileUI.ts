32719b45eaa174642df357b6bfc17a1593a01865
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-11-21 21:46:21 +0800

    默认自动识别 Android / iOS 设备

--- client/ui/isMobileUI.ts
+++ client/ui/isMobileUI.ts
@@ -1,3 +1,3 @@
 export default function isMobileUI() {
-    return new URL(location.href).searchParams.get('mobile') == 'true'
+    return new URL(location.href).searchParams.get('mobile') == 'true' || /Mobi|Android|iPhone/i.test(navigator.userAgent)
 }

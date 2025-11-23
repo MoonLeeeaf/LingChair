7fcf4ce50bbb3c5c1a88c0638235837510386b11
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-11-23 16:35:18 +0800

    idk

--- mod.ts
+++ mod.ts
@@ -1,8 +1,6 @@
-import createLingChairServer from './server/server.ts'
 import LingChairClient from './client-protocol/LingChairClient.ts'
 
 export * from './client-protocol/LingChairClient.ts'
 export {
-    createLingChairServer,
     LingChairClient
 }

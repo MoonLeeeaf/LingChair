6ca9946499837c25d1150203adc54a18c6638065
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-12-06 01:39:47 +0800

    和deno斗争

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,40 +1,12 @@
 import { defineConfig } from 'vite'
-import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
 import config from '../server/config.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), deno()],
+    plugins: [react()],
     build: {
         sourcemap: true,
         outDir: "." + config.data_path + '/page_compiled',
-        minify: 'terser',
-        cssMinify: 'lightningcss',
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('mdui')) {
-                            return 'mdui'
-                        }
-                        if (id.includes('crypto-js')) {
-                            return 'cryptojs'
-                        }
-                        if (id.includes('split.js')) {
-                            return 'splitjs'
-                        }
-                        if (id.includes('marked')) {
-                            return 'marked'
-                        }
-                        return 'vendor'
-                    }
-                    return 'main'
-                },
-            }
-        }
-    },
-    optimizeDeps: {
-        include: ['socket.io-client']
     }
 })
--- mod.ts
+++
@@ -1,6 +0,0 @@
-import LingChairClient from './client-protocol/LingChairClient.ts'
-
-export * from './client-protocol/LingChairClient.ts'
-export {
-    LingChairClient
-}

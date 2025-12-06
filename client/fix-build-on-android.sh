02b0708426fa007b098f4d4d877e940e488a14dd
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-12-06 11:08:39 +0800

    修改项目配置

--- client/fix-build-on-android.sh
+++
@@ -1,3 +0,0 @@
-rm -r ./node_modules/.deno/rollup@4.50.1/node_modules/rollup/
-cp -r ./node_modules/.deno/@rollup+wasm-node@4.48.0/node_modules/@rollup/wasm-node/ node_modules/.deno/rollup@4.50.1/node_modules/rollup/
-echo Replaced rollup with @rollup/wasm-node successfully

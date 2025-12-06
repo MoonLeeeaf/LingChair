f57347b83450b00333293e4cf9ed1020cd168e76
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-12-06 17:00:58 +0800

    共享上下文

--- client/ui/MainSharedContext.ts
+++ client/ui/MainSharedContext.ts
@@ -1,5 +1,12 @@
 import { createContext } from 'react'
 
-const MainSharedContext = createContext({})
+type shared = {
+    ui_functions: React.MutableRefObject<{
+
+    }>
+    setShowLoginDialog: React.Dispatch<React.SetStateAction<boolean>>
+    setShowRegisterDialog: React.Dispatch<React.SetStateAction<boolean>>
+}
+const MainSharedContext = createContext({} as shared)
 
 export default MainSharedContext

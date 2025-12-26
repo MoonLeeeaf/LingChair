3b3e9a3d9d3542199671a0346507f68c58aa21c3
CrescentLeaf <crescentleaf@noreply.codeberg.org>
2025-12-26 21:18:56 +0800

    test

--- client/ui/routers/useRouterDialogRef.ts
+++ client/ui/routers/useRouterDialogRef.ts
@@ -6,28 +6,10 @@ import * as React from 'react'
 
 export default function useRouterDialogRef() {
     const dialogRef = React.useRef<Dialog>()
-    const proceedRef = React.useRef<() => void>()
-    const shouldBlock = React.useRef(true)
-    const nav = useNavigate()
-    const blocker = useBlocker(React.useCallback<BlockerFunction>(() => shouldBlock.current, []))
-
-    // 避免用户手动返回导致动画丢失
-    React.useEffect(() => {
-        if (blocker.state === "blocked") {
-            proceedRef.current = blocker.proceed
-            // 这个让姐姐来就好啦
-            dialogRef.current!.open = false
-        }
-    }, [blocker.state])
 
     useAsyncEffect(async () => {
         await sleep(10)
         dialogRef.current!.open = true
-        dialogRef.current!.addEventListener('closed', async () => {
-            shouldBlock.current = false
-            await sleep(10)
-            proceedRef.current ? proceedRef.current() : nav(-1)
-        })
     }, [])
     return dialogRef
 }
